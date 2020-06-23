6ceb2fadf7dcb9bf91d802056741ae6e72f18a98
Julian Quintana <julianestebanquintana@gmail.com>
2020-06-23 09:59:53 -0500

    First question answered

--- sakila_query.sql
+++ sakila_query.sql
@@ -13,8 +13,21 @@ Create a query that lists each movie, the film category it is classified in,
 and the number of times it has been rented out.
 */
 
-
-
+SELECT film.title AS movie,
+       cat.name AS classification,
+       count(rent.rental_id) AS times_rented
+  FROM category AS cat
+  JOIN film_category AS fcat
+    ON cat.category_id = fcat.category_id
+  JOIN film
+    ON fcat.film_id = film.film_id
+  JOIN inventory AS inv
+    ON film.film_id = inv.film_id
+  JOIN rental AS rent
+    ON inv.inventory_id = rent.inventory_id
+ WHERE cat.name IN ('Animation', 'Children', 'Classics', 'Comedy', 'Family', 'Music')
+GROUP BY movie, classification
+ORDER BY classification, movie;
 
 
 /*  
@@ -79,4 +92,20 @@ terms of payments.
 
 
 
+/* 
+PROJECT SUBMISSION
+You are now on the portion of the project you will need to submit to a reviewer. 
+To pass this project follow the instructions below to create a presentation. 
+You will submit a zip file with two items:
+- Slide Deck (4 slides)
+- Text File with SQL queries
+Your presentation should include:
+- Four slides
+- One question on each slide
+- One visualization (graph / chart / table) per slide
+- A 1-2 sentence answer to the question, based on the data and visualization, on each slide
+
+RUBRIC!!!
+https://review.udacity.com/#!/rubrics/2095/view
+*/
 
